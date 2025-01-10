0d4131cbeeb4d64391ba65a7da5091e97429ee06
raisatma79 <raisatmaramadhan@gmail.com>
2025-01-10 17:21:58 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "modul9";
+
+$koneksi = mysqli_connect($server, $username, $password, $database) or die(mysqli_error($koneksi));
+?>
